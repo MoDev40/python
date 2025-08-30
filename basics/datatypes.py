9eb8f29cc7d9d69b06b8ac633d969b876fc4349a
Muqtaar Axmed Maxamed <143383104+MoDev40@users.noreply.github.com>
2025-08-30 06:56:31 +0300

    Refactor datatypes.py to focus on string operations
    Commented out previous datatype examples and added comprehensive demonstrations of string operations, including indexing, slicing, formatting, and built-in string methods. This refactor shifts the file's focus from general Python datatypes to string manipulation and usage.

--- basics/datatypes.py
+++ basics/datatypes.py
@@ -1,37 +1,85 @@
-x = int(100)
-y = float(20.0)
-left = complex(2j)
-countries = list(('Somalia','Yemen','Falestiin'))
-cities = tuple(('Mogadisho','London'))
-count = range(10)
-json = dict(name="Mdev",age=33)
-fruits = set(('Mango','Apple'))
-freezset = frozenset(('Freeze','Cold'))
-istrue = bool(False)
-byte_array = bytearray(10)
-string = str('Mdev')
+# x = int(100)
+# y = float(20.0)
+# left = complex(2j)
+# countries = list(('Somalia','Yemen','Falestiin'))
+# cities = tuple(('Mogadisho','London'))
+# count = range(10)
+# json = dict(name="Mdev",age=33)
+# fruits = set(('Mango','Apple'))
+# freezset = frozenset(('Freeze','Cold'))
+# istrue = bool(False)
+# byte_array = bytearray(10)
+# string = str('Mdev')
+#
+# print(countries)
+# print(cities)
+# print(count)
+# print(json)
+# print(fruits)
+# print(freezset)
+# print(istrue)
+# print(byte_array)
+# print(string)
+#
+#
+# teams = ['Man City','Barca','PSG']
+# print(teams)
+#
+# players = ('Ryan','Pedri','Pacho')
+# print(type(players))
+#
+# coaches = [{'name':'Josep','age':54},{'name':'Gasperini','age':74}]
+# print(coaches)
+# coaches.append({'name':'Jose','age':54})
+# print(coaches)
+# refs = frozenset(('Ryan','Pedro','Paco'))
+# print(refs)
+# print(type(refs))
+from itertools import count
 
-print(countries)
-print(cities)
-print(count)
-print(json)
-print(fruits)
-print(freezset)
-print(istrue)
-print(byte_array)
-print(string)
+# String
 
+vehicle = "Tesla"
+print(vehicle)
 
-teams = ['Man City','Barca','PSG']
-print(teams)
+multiline = """Once a time there is a such a young age boy who doesn't need anymore to stay along life. """
+print(multiline)
 
-players = ('Ryan','Pedri','Pacho')
-print(type(players))
+print(vehicle[0])
 
-coaches = [{'name':'Josep','age':54},{'name':'Gasperini','age':74}]
-print(coaches)
-coaches.append({'name':'Jose','age':54})
-print(coaches)
-refs = frozenset(('Ryan','Pedro','Paco'))
-print(refs)
-print(type(refs))
+for x in vehicle:
+	print(x)
+
+print(len(vehicle))
+
+print("T" in vehicle)
+
+print("V" not in vehicle)
+
+vehicle += ',Its time to buy'
+
+print(vehicle[2:4])
+print(vehicle[2:])
+print(vehicle[:3])
+print(vehicle.isupper())
+print(vehicle.lower())
+print(vehicle.title())
+print(vehicle.strip())
+print(vehicle.split(','))
+
+print(f"The price of this phone is around ${1500:.2f} dollars")
+print(f"My name is {vehicle.split(',')[0].title()} and I am modern vehicles")
+
+print("can i get this little \"toy\".")
+print("So far so good \tMove away")
+print("So much love new lines \nHi")
+print("What is mean\\")
+print("What return can do for me \rHello where the previous line.😒")
+
+define_age = "Can we just say age isn\'t number"
+print(define_age.count('we'))
+print(define_age.encode())
+print(define_age.isnumeric())
+print(define_age.isalnum())
+print(define_age.isalpha())
+print(define_age.isascii())
