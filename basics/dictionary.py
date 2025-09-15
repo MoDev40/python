603a716998b74409d201d979c98dd1031b3cb79a
Muqtaar Axmed Maxamed <143383104+MoDev40@users.noreply.github.com>
2025-09-15 11:21:57 +0300

    Add examples demonstrating Python dictionary usage
    Introduces basics/dictionary.py with examples of creating, accessing, updating, and inspecting Python dictionaries. Includes demonstrations of dictionary methods and key existence checks.

---
+++ basics/dictionary.py
@@ -0,0 +1,35 @@
+car = {
+	'brand':'Ford',
+	'model':'Mustang',
+	'year':1964
+}
+print(car)
+print(type(car))
+print(car.keys())
+print(car.values())
+print(car['year'])
+print(car['model'])
+print(len(car))
+
+student = {
+	'name':'Osama',
+	'age':23,
+	'courses':['Python','JS','React'],
+	'is_active':True,
+}
+
+keys = student.keys()
+values = student.values()
+
+print(student)
+print(student.get('courses'))
+print(student['is_active'])
+print('before update',[keys,values])
+student['location'] = 'KM4'
+student['is_active'] = False
+print('after update',[keys,values])
+print('Items ',student.items())
+if 'is_active' in student:
+	print('is_active key is in student')
+
+print(dict(color='Red',type='Electric',price=20,period='2m'))
