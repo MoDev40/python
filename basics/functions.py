8b31c5ad99e555c6a5e35aa9083aced5a3dfc492
Muqtaar Axmed Maxamed <143383104+MoDev40@users.noreply.github.com>
2025-09-17 14:56:45 +0300

    Add various function examples and demonstrations
    Implemented multiple function examples in functions.py, including basic greetings, argument handling (positional, keyword, *args, **kwargs), default parameters, and a max value function. These examples demonstrate different ways to define and call functions in Python.

--- basics/functions.py
+++ basics/functions.py
@@ -22,7 +22,79 @@ except ValueError as e:
     print(e)
 
 
-def calculate_weekly_salary(
-    days_attended: int, hr_attended: int, per_hr: float
-) -> float:
-    pass
+def say_hi():
+    print("Hi everyone!")
+
+
+say_hi()
+
+
+def say_hello(f_name):
+    print(f"Hello, {f_name}")
+
+
+say_hello("John")
+say_hello("Alia")
+
+
+def student(name, age):
+    print(f"Hello, {name}, you are {age} years old.")
+
+
+student("John", 25)
+
+
+def print_hobbies(*hobbies):
+    print(hobbies)
+
+
+print_hobbies(1, 2, 3, 4)
+
+
+def show_attends(*args):
+    print(f"Total attends: {len(args)}")
+    for attend in args:
+        print(f"Hello {attend} welcome we are happy to see you.")
+
+
+show_attends("John", "Alia", "Jane", "Hani", "Omar")
+
+student(age=33, name="Aisha")
+
+
+def team_announcement(**kwargs):
+    print(kwargs)
+
+
+team_announcement(alia="Alia", hassan="Hassan", hani="Hani")
+
+
+def is_active(active=True):
+    print("Active" if active else "Inactive")
+
+
+is_active(False)
+is_active()
+
+
+def max_value(data: list) -> float:
+    return max(data)
+
+
+print(max_value([1, 2, 3]))
+
+
+def positional_arg_only(a, b, /):
+    print(a + b)
+
+
+positional_arg_only(3, 6)
+positional_arg_only(3, 6)
+
+
+def keyword_arg_only(*, a, b):
+    print(a+b)
+
+
+keyword_arg_only(a=6, b=8)
+keyword_arg_only(a=7,b=4)
