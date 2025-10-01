cd125974d7aeaf04eb7cbf044f223186b045a1f0
Muqtaar Axmed Maxamed <143383104+MoDev40@users.noreply.github.com>
2025-10-01 10:39:47 +0300

    Add basic math operations example script
    Introduces _math.py with examples of built-in and math module functions such as max, min, abs, pow, sqrt, ceil, floor, and factorial.

---
+++ basics/_math.py
@@ -0,0 +1,12 @@
+ages = [19, 21, 34, 40, 51, 20, 17]
+print(max(ages))
+print(min(ages))
+print(abs(-7))
+print(abs(-11))
+print(pow(3,4))
+print(pow(2,3))
+import  math
+print(math.sqrt(64))
+print(math.ceil(8.54))
+print(math.floor(8.54))
+print(math.factorial(7))
