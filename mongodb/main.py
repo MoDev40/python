88bddb9e82df25c639438715c9acaea5ef53b99b
Muqtaar Axmed Maxamed <modev.404@gmail.com>
2025-11-12 20:18:05 +0300

    insert data to the collection insertOne and Many

--- mongodb/main.py
+++ mongodb/main.py
@@ -1,19 +1,44 @@
 import pymongo
 
-mongo = pymongo.MongoClient('mongodb://localhost:27017/')
+mongo = pymongo.MongoClient("mongodb://localhost:27017/")
 
 db_list = mongo.list_database_names()
-db_name = 'python'
+db_name = "basics"
 
-if not db_name in db_list:
-    global db
-    db = mongo[db_name]
-    print(f'DB {db_name} not exist')
 
-customers = 'customers'
+db = mongo[db_name]
+
+customers = "customers"
 collections = db.list_collection_names()
 print(collections)
 
-if not customers in collections:
-    print(f'Collection {customers} not exists')
-    db[customers]
+customer = db[customers]
+
+def insertOne(data: dict):
+    x = customer.insert_one(data)
+    print(x)
+
+def insetMany(data:list[dict]):
+    x = customer.insert_many(data)
+    print(x)
+
+# insertOne({"name": "John Doe", "address": "L33KM2"})
+
+# insetMany([{'name':'Gomez',"address": "Mog,KM4"}])
+
+my_customers =  [
+  { "name": "Amy", "address": "Apple st 652"},
+  { "name": "Hannah", "address": "Mountain 21"},
+  { "name": "Michael", "address": "Valley 345"},
+  { "name": "Sandy", "address": "Ocean blvd 2"},
+  { "name": "Betty", "address": "Green Grass 1"},
+  { "name": "Richard", "address": "Sky st 331"},
+  { "name": "Susan", "address": "One way 98"},
+  { "name": "Vicky", "address": "Yellow Garden 2"},
+  { "name": "Ben", "address": "Park Lane 38"},
+  { "name": "William", "address": "Central st 954"},
+  { "name": "Chuck", "address": "Main Road 989"},
+  { "name": "Viola", "address": "Sideway 1633"}
+]
+
+# insetMany(my_customers)
